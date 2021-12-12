ab94419fc3c6e27dc1a2d1ea1edc8c29367b750e
Israel Garcia Mesa <israelg@microsoft.com>
2021-12-12 01:40:55 +0100

    Fix Get-Location for Connectors

--- src/Dev/Framework/Microsoft.Sentinel.Connectors/Version/Microsoft.Sentinel.Connectors/Microsoft.Sentinel.Connectors.psm1
+++ src/Dev/Framework/Microsoft.Sentinel.Connectors/Version/Microsoft.Sentinel.Connectors/Microsoft.Sentinel.Connectors.psm1
@@ -1,4 +1,4 @@
 function Get-AzSentinelConnectorsLocation{
-    $ConnectoreModuleBasePath = (Get-Module -Name Microsoft.Sentinel.Connectors).Path | Split-Path
+    $ConnectoreModuleBasePath = (Get-Module -Name Microsoft.Sentinel.Connectors -ListAvailable).Path | Split-Path -Parent
     return Join-Path -Path $ConnectoreModuleBasePath -ChildPath "Connectors"
 }
